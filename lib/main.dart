455d2a7f92dd7d90013efadef7e775dc1c317408
alyamih <alevtina.mikhailova@gmail.com>
2024-02-16 13:45:28 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:financial_app/pages/splash_page.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+int? initScreen;
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences preferences = await SharedPreferences.getInstance();
+  initScreen = preferences.getInt('initScreen');
+
+  await preferences.setInt('initScreen', 1);
+  //preferences.clear();
+  initializeDateFormatting('en_US');
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: false,
+      ),
+      home: const SplachScreen(),
+    );
+  }
+}
